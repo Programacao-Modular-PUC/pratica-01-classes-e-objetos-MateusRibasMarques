5c4528e32beb40e406d3c5fac1d34934fc7a362e
1583479 <1583479@PMG34INFLB20718.PUCMINAS.NET>
2026-03-05 18:29:56 -0300

    Criaçao Inicial

---
+++ Project/src/project/Prática01.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package project;
+import java.util.Scanner;
+/**
+ *
+ * @author 1583479
+ */
+ 
+public class Prática01 {
+
+        public static void main(String[] args) {
+            
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o nome: ");
+        String nome = sc.nextLine();
+        
+        System.out.print("Digite o Sobrenome: ");
+        String Sobrenome = sc.nextLine();
+        
+        System.out.print("Digite a idade: ");
+        int idade = sc.nextInt();
+        
+         System.out.print("Digite a altur: ");
+        float altura = sc.nextInt();
+        
+         System.out.print("Digite o peso: ");
+        float peso = sc.nextInt();
+        
+        
+        
+    }
+}
+
+  
+
+    
+
