74232df922443295d4f482bfc131fed5b006ab95
Mateus Ribas <marcalmateus315@gmail.com>
2026-03-10 14:38:33 -0300

    mat

--- Project/src/project/Pratica02.java
+++ Project/src/project/Pratica02.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package project;
 
-/**
- *
- * @author Windows 10
- */
+import java.util.Scanner;
+
 public class Pratica02 {
-    
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        TrianguloRetangulo t = new TrianguloRetangulo();
+        
+        System.out.print("Digite o cateto 1: ");
+        float c1 = sc.nextFloat();
+        
+        System.out.print("Digite o cateto 2: ");
+        float c2 = sc.nextFloat();
+        
+        t.setc1(c1);
+        t.setc2(c2);
+        
+        float hipotenusa = t.CalculaHipotenusa();
+        float area = t.CalculaArea();
+        
+        System.out.println("Hipotenusa: " + hipotenusa);
+        System.out.println("Área do triângulo: " + area);
+        
+    }
 }
--- Project/src/project/TrianguloRetangulo.java
+++ Project/src/project/TrianguloRetangulo.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package project;
 
-/**
- *
- * @author Windows 10
- */
 public class TrianguloRetangulo {
     
+    private float h;
+    private float c1;
+    private float c2;
+
+    public float getc1() {
+        return c1;
+    }
+
+    public float getc2() {
+        return c2;
+    }
+
+    public float geth() {
+        return h;
+    }
+
+    public void setc1(float c) {
+        c1 = c;
+    }
+
+    public void setc2(float cc) {
+        c2 = cc;
+    }
+
+    public float CalculaHipotenusa() {
+        h = (float) Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2));
+        return h;
+    }
+
+    public float CalculaArea() {
+        float area = (c1 * c2) / 2;
+        return area;
+    }
 }
